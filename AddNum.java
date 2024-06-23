f04d8d6f92ca737810ac106e30fc80ce4da7e012
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-24 00:42:17 +0530

    this is my commit

---
+++ AddNum.java
@@ -0,0 +1,17 @@
+public class AddNum{
+
+public static void main(String[] args){
+
+int num1=10;
+int num2=10;
+int sum=num1+num2;
+
+if(sum==num1){
+System.out.println("the sum is equal to num1");
+}else{
+System.out.println("the sum is not equal to num1");
+}
+}
+}
+
+
