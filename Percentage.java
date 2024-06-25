59d9c9b3f1532f575bae5bc74841bfe87217d96c
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 12:06:55 +0530

    Array in double

---
+++ Percentage.java
@@ -0,0 +1,13 @@
+public class Percentage{
+public static void main(String args[]){
+
+double marks[]={98.65, 56.98, 83.76, 90.87,69.70};
+for(int i=0; i<marks.length; i++){
+
+System.out.println("the Percentage is:" + marks[i]);
+}
+System.out.println("the length of marks :" + marks.length);
+
+
+}
+}
