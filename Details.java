cec252964d91bfa173ebb48a47d50040fd26c53a
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 12:22:03 +0530

    Array in String

---
+++ Details.java
@@ -0,0 +1,13 @@
+public class Details{
+public static void main(String args[]){
+
+String info[]={"xyz", "10 june", "sat","bengaluru" };
+for(int i=0; i<info.length; i++){
+
+System.out.println("the Information is:" + info[i]);
+}
+System.out.println("the length of info :" + info.length);
+
+
+}
+}
