c8a23aee8184a676e756360debecb0d7bdd3c0df
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 12:22:56 +0530

    Array in String

---
+++ Grade.java
@@ -0,0 +1,13 @@
+public class Grade{
+public static void main(String args[]){
+
+char value[]={'A', 'B','c', 'D'};
+for(int i=0; i<value.length; i++){
+
+System.out.println("the garde is:" + value[i]);
+}
+System.out.println("the length of grade :" + value.length);
+
+
+}
+}
