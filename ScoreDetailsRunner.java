3f31be31d07a0c98eba9c2c0d16596c92d59217b
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 22:54:46 +0530

    Array using Static

---
+++ ScoreDetailsRunner.java
@@ -0,0 +1,17 @@
+public class ScoreDetailsRunner{
+
+public static void main(String args[]){
+	
+	for(int i=0; i<ScoreDetails.points.length; i++){
+	
+	System.out.println(" the points are:" + ScoreDetails.points[i]);
+	//System.out.println(" the Team name:" + ScoreDetails.name[]);
+	//System.out.println(" the date is:" + ScoreDetails.date[]);
+	}
+}
+}
+
+	
+	
+
+
