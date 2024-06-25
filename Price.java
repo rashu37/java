14e03178fa9c78e7481b274f568c75575181a348
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 12:18:57 +0530

    Array in Short

---
+++ Price.java
@@ -0,0 +1,13 @@
+public class Price{
+public static void main(String args[]){
+
+short cost[]={1500,2000,4000, 7220,9000};
+for(int i=0; i<cost.length; i++){
+
+System.out.println("the Price is:" + cost[i]);
+}
+System.out.println("the length of cost :" + cost.length);
+
+
+}
+}
