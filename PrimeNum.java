4ed0893693be9ddf5ba44fca1c32407bb8e983a6
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 22:57:57 +0530

    uaing array

---
+++ PrimeNum.java
@@ -0,0 +1,12 @@
+public class PrimeNum{
+    public static void main(String args[]){
+      int elements[]={2,3,5,11,13,17,19,23,29};
+          
+          for(int i=elements.length-1; i>=0; i--){
+	      
+	           
+                 System.out.println(" "+ "the elements are" + elements[i] +"  " );
+                
+		  }           
+			   }
+}
