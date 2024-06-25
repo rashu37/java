26107e7e895e164b958382fcde9a9a3a1f52289f
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 12:16:07 +0530

    Array in float

---
+++ Temperature.java
@@ -0,0 +1,13 @@
+public class Temperature{
+public static void main(String args[]){
+
+float value[]={23.5F, 13.76F,17.98F, 34.43F};
+for(int i=0; i<value.length; i++){
+
+System.out.println("the Temperature value is:" + value[i]);
+}
+System.out.println("the length of value :" + value.length);
+
+
+}
+}
