c3ace5d691582c4eef9048c741df1952472f6f68
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 12:20:10 +0530

    Array in Long

---
+++ PhoneNum.java
@@ -0,0 +1,13 @@
+public class PhoneNum{
+public static void main(String args[]){
+
+long num[]={98765076L, 9876543765L, 9876543776L};
+for(int i=0; i<num.length; i++){
+
+System.out.println("the Phone Number is:" + num[i]);
+}
+System.out.println("the length of num :" + num.length);
+
+
+}
+}
