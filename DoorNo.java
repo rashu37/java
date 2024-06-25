7ba8b7961b7dde88684ea24d3dd87f06864f360f
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 12:17:19 +0530

    Array in Byte

---
+++ DoorNo.java
@@ -0,0 +1,13 @@
+public class DoorNo{
+public static void main(String args[]){
+
+byte value[]={10,21,37,19,100};
+for(int i=0; i<value.length; i++){
+
+System.out.println("the DoorNo is:" + value[i]);
+}
+System.out.println("the length of Value :" + value.length);
+
+
+}
+}
