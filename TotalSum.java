774edb54fdefed7efbf2710fc0d0bb2c65757414
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 22:55:55 +0530

    using array

---
+++ TotalSum.java
@@ -0,0 +1,13 @@
+public class TotalSum{
+    public static void main(String args[]){
+      int num[]={3,8,32,41,6,8};
+        int sum=0;
+               for(int index=0; index<num.length; index++){
+               sum=sum+num[index];
+			   
+			   }
+                 System.out.println("the total sum is:" + sum);
+
+                
+     }
+}
