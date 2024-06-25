59ccecf06df36cc0057b4ff4fcead22fb110efef
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-25 22:47:57 +0530

    array using static

---
+++ MenuDetailsRunner.java
@@ -0,0 +1,10 @@
+public class MenuDetailsRunner{
+
+ public static void main(String args[]){
+ 
+ for(int index=0; index<MenuDetails.snakcs.length; index++){
+ 
+ System.out.println("The menu of snacks:" + MenuDetails.snakcs[index]);
+ }
+ }
+ }
