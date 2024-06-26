2eefff9cccaf0f0a471bc7df030f42dde1c4dc93
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-26 22:42:05 +0530

    26/6 Vowels

---
+++ Vowels.java
@@ -0,0 +1,18 @@
+public class Vowels{
+
+public static void main(String args[]){
+
+char elements[]={'A', 'E','I','O','U'};
+
+for(int i=0; i<elements.length; i++){
+
+System.out.println("The vowels are :" + elements[i]);
+
+}
+System.out.println("Total :" + elements.length);
+
+
+
+}
+
+}
