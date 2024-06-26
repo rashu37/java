40bf9fd8252f39d9ee715b337ec9285bccb2d687
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-26 22:47:26 +0530

    cityRunner

---
+++ CityRunner.java
@@ -0,0 +1,21 @@
+public class CityRunner{
+public static void main(String args[]){
+
+String city1[]= {"Mysore","Bengaluru"};
+String city3="null";
+
+for(int i=0; i<city1.length; i++){
+    city3= city3 + City.name[i]
+
+System.out.println("the city is : " + city3);
+
+}
+}
+ 
+
+
+
+
+
+
+}
