de695b17b4f4af60f8ce8d16faa87864e6434ff1
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-26 22:43:24 +0530

    26/6 Consonants

---
+++ Consonants.java
@@ -0,0 +1,18 @@
+public class Consonants{
+
+public static void main(String args[]){
+
+char elements[]={'B','C', 'D', 'F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'};
+
+for(int i=0; i<elements.length; i++){
+
+System.out.println("The Consonants are :" + elements[i]);
+
+}
+System.out.println("Total  are :" + elements.length);
+
+
+
+}
+
+}
