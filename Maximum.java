52441d3bbfe6b95d47ff9f4a70946e6952cfb5d2
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-27 10:29:23 +0530

    Maximum

---
+++ Maximum.java
@@ -0,0 +1,17 @@
+public class Maximum{
+public static void main(String args[]){
+
+System.out.println("the maximum values of datatypes are :");
+
+System.out.println("Byte range: " + Byte.MAX_VALUE);
+System.out.println("Int range: " + Integer.MAX_VALUE);
+System.out.println("Long range: " + Long.MAX_VALUE);
+System.out.println("Short range: " + Short.MAX_VALUE);
+System.out.println("Double range: " + Double.MAX_VALUE);
+System.out.println("Float range: " + Float.MAX_VALUE);
+
+
+
+}
+}
+
