3741ffe4919f93fe85922d568bdc930801a60a03
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-27 10:04:00 +0530

    cube

---
+++ Cube.java
@@ -0,0 +1,18 @@
+public class Cube{
+public static void main(String args[]){
+
+
+int number=2;
+
+int num= number*number*number;
+
+
+System.out.println(" the number of cube is: " + num);
+
+
+}
+
+
+
+
+}
