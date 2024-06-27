487a1edda80d56244568852a0d8b2047d3c08a57
rashu37 <rashmi10.xworkz@hmail.com>
2024-06-27 10:24:00 +0530

    Minimum vlaues of datatypes

---
+++ Minimum.java
@@ -0,0 +1,17 @@
+public class Minimum{
+public static void main(String args[]){
+
+System.out.println("the minimum values of datatypes are :");
+
+System.out.println("Byte range: " + Byte.MIN_VALUE);
+System.out.println("Int range: " + Integer.MIN_VALUE);
+System.out.println("Long range: " + Long.MIN_VALUE);
+System.out.println("Short range: " + Short.MIN_VALUE);
+System.out.println("Double range: " + Double.MIN_VALUE);
+System.out.println("Float range: " + Float.MIN_VALUE);
+
+
+
+}
+}
+
