87d8a77c1f94e1140537124190c35391f18ada5f
rashu37 <rashmi10.xworkz@hmail.com>
2024-07-09 22:48:53 +0530

    9/7 method overloading

---
+++ Method overloading/ApplicationRunner.java
@@ -0,0 +1,18 @@
+public class ApplicationRunner{
+
+  public static void main(String args[]){
+  
+  Application.apply("Seetha");
+  Application.apply(21, "10 nov");
+  System.out.println(Application.apply("koppal", 7874817693L));
+  
+  System.out.println(Application.apply("bengaluru", 56, 9763545678L));
+  
+  System.out.println(Application.apply("five",60000));
+  
+  System.out.println(Application.apply("Koppal", 75345627262L, 22));
+  
+  
+  }
+}
+  
